fbb7069552f910e2b29dd617af4ba87e532a0c69
Pranav Pooruli <pranav.pooruli@gmail.com>
2020-06-15 18:33:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='ip',  # Replace with your own username
+    name='pythonscriptgetip',  # Replace with your own username
     version='0.0.1',
     author='Pranav Balaji Pooruli',
     author_email='pranav.pooruli@gmail.com',
     description='A small package that lets you detect both of your IP addresses through Python script.',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/Python3-8/IP',
+    url='https://github.com/Python3-8/PYTHONSCRIPTGETIP',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
